d945a19e596138ddf4c74761cb178cb14bae8ab8
alihikmet <ahikmetdemiray@gmail.com>
2022-04-28 15:38:26 -0400

    aq

--- hop/src/Test.java
+++ hop/src/Test.java
@@ -2,9 +2,20 @@
 
 public class Test {
 
+    static {
+        System.out.println("hi");
+    }
+    {
+        System.out.println(gij.i);
+        System.out.println("Hello");
+    }
+    Test(){
+        gij.i=30;
+        System.out.println("SJ");
+    }
+
     public static void main(String[] args) {
-
-        System.out.println("Hello Git");
-
+        System.out.println("ciao");
+        Test t=new Test();
     }
 }
---
+++ hop/src/gij.java
@@ -0,0 +1,10 @@
+public class gij {
+
+    static int i = 20;
+    static void play(){
+        System.out.println("play");
+    }
+    gij(){
+        System.out.println("AMCIK");
+    }
+}
